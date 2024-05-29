8b153690db4cef1b068e17dab1f570d7be042107
z3z1ma <butler.alex2010@gmail.com>
2024-05-29 03:09:54 -0700

    fix: correct ttl logic

--- src/harness_tui/utils.py
+++ src/harness_tui/utils.py
@@ -2,18 +2,18 @@ import time
 from functools import lru_cache, wraps
 
 
-def ttl_cache(ttl: int):
+def ttl_cache(ttl_seconds: int):
     def compute_hash():
-        round(time.time() / ttl)
+        round(time.time() / ttl_seconds)
 
     def decorator(func):
         @wraps(func)
+        @lru_cache(maxsize=10)
         def func_wrapper(*args, ttl_hash=None, **kwargs):
             _ = ttl_hash
             return func(*args, **kwargs)
 
         @wraps(func)
-        @lru_cache(maxsize=10)
         def with_ttl(*args, **kwargs):
             return func_wrapper(*args, **kwargs, ttl_hash=compute_hash())
 
