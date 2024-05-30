fe67bb9379ae0d9e7d85614ea6528fea00d5000f
prasannaHar <prasanna.vaddkkepurakkal@harness.io>
2024-05-30 01:30:17 -0700

    update vectordb id

--- src/harness_tui/vectordb/interface.py
+++ src/harness_tui/vectordb/interface.py
@@ -8,6 +8,7 @@ import numpy as np
 
 
 class VectorDatabase:
+
     def __init__(self, uri, vector_size=128):
         self.uri = uri
         self.vector_size = vector_size
@@ -27,18 +28,21 @@ class VectorDatabase:
         if self.async_db is None:
             raise RuntimeError("Database is not connected.")
         if not await self.table_exists(table_name):
-            self.async_tbl = await self.async_db.create_table(table_name, data=data)
+            self.async_tbl = await self.async_db.create_table(table_name,
+                                                              data=data)
         else:
             self.async_tbl = await self.async_db.open_table(table_name)
 
     def hash_to_vector(self, s: str):
         hash_bytes = hashlib.sha256(s.encode("utf-8")).digest()
-        hash_vector = np.frombuffer(hash_bytes, dtype=np.uint8)[: self.vector_size]
+        hash_vector = np.frombuffer(hash_bytes,
+                                    dtype=np.uint8)[:self.vector_size]
         return hash_vector.tolist()
 
     def get_log_dict_from_directory(self, directory_path: str):
         log_list = []
         for root, _, files in os.walk(directory_path):
+            print("root",root)
             for file in files:
                 if file != ".DS_Store":
                     full_path = os.path.join(root, file)
@@ -47,15 +51,19 @@ class VectorDatabase:
 
                     try:
                         log_entries = [
-                            json.loads(line) for line in content.strip().split("\n")
+                            json.loads(line)
+                            for line in content.strip().split("\n")
                         ]
                         obj = {
-                            "vector": self.hash_to_vector(full_path),
+                            "vector": self.hash_to_vector(full_path+str(log_entries)),
                             "log_content": str(log_entries),
                         }
                         log_list.append(obj)
+                        print(obj)
                     except json.JSONDecodeError:
-                        print(f"The file content in {full_path} is not valid JSON.")
+                        print(
+                            f"The file content in {full_path} is not valid JSON."
+                        )
                         print(content)
 
         return log_list
@@ -63,26 +71,25 @@ class VectorDatabase:
     async def vector_search(self, query: str, limit: int = 2):
         if self.async_tbl is None:
             raise RuntimeError("Table is not created.")
-        return (
-            await self.async_tbl.vector_search(self.hash_to_vector(query))  # type: ignore
-            .limit(limit)
-            .to_pandas()
-        )
+        return (await self.async_tbl.vector_search(self.hash_to_vector(query)
+                                                   )  # type: ignore
+                .limit(limit).to_pandas())
 
 
-# Usncomment to test code
-# async def main():
-#     uri = "data/sample-lancedb"
-#     db = VectorDatabase(uri)
-#     await db.connect()
+# Uncomment to test code
+async def main():
+    uri = "data/sample-lancedb"
+    db = VectorDatabase(uri)
+    await db.connect()
 
-#     directory_path = "./logs"
-#     log_list = db.get_log_dict_from_directory(directory_path)
-#     await db.create_table("vector_idss", log_list)
+    directory_path = "./logs/"
+    log_list = db.get_log_dict_from_directory(directory_path)
+    print("log_list",log_list)
+    await db.create_table("vector_idsss", log_list)
 
-#     search_result = await db.vector_search("Aqua")
-#     print(search_result)
+    search_result = await db.vector_search("Aqua")
+    print(search_result)
 
-# # Running the async main function
-# import asyncio
-# asyncio.run(main())
+# Running the async main function
+import asyncio
+asyncio.run(main())
